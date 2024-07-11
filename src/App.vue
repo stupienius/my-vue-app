abcbaf649b6d968491d20a6d3716c1dbd101ade9
stupienius <wangjay0401@gmail.com>
2024-07-11 16:10:15 +0800

    library

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,40 @@
 <script setup>
-	import Selection from './components/menu.vue'
-	import Banner from './components/banner.vue'
-	import Add from './components/add.vue'
+import Selection from './components/menu.vue'
+import Banner from './components/banner.vue'
+import Add from './components/add.vue'
+import Library from './components/library.vue'
 </script>
 
 <template>
-	<body>		
+	<body>
 		<header>
 			<Banner/>
 		</header>
 		<main>
 			<Selection/>
-			<Add />
+			<Add/>
+			<Library/>
 		</main>
 	</body>
 </template>
 
 <style scoped>
-	body{
-		display: flex;
-		flex-direction: row;
-	}
-	header{
-		display: flex;
-		flex-direction: column;
-	}
-	.menu{
-		height: auto;
-	}
-	main{
-		display: flex;
-		flex-direction: column;
-		background-color: gray;	
-	}
+body {
+	display: flex;
+	flex-direction: row;
+}
+
+header {
+	display: flex;
+	flex-direction: column;
+}
+
+.menu {
+	height: auto;
+}
+
+main {
+	display: flex;
+	flex-direction: column;
+}
 </style>
--- src/components/library.vue
+++ src/components/library.vue
@@ -1,9 +1,41 @@
 <script setup>
+import { ref, onMounted } from 'vue';
 
+const vocabularyList = ref([]);
+
+const loadFromLocalStorage = () => {
+    const savedList = localStorage.getItem('vocabularyList');
+    if (savedList) {
+        vocabularyList.value = JSON.parse(savedList);
+    }
+};
+
+onMounted(() => {
+    loadFromLocalStorage();
+});
 </script>
+
 <template>
-
+    <div>
+        <h2>Vocabulary List</h2>
+        <ul v-if="vocabularyList.length > 0">
+            <li v-for="(entry, index) in vocabularyList" :key="index">
+                <strong>{{ entry.word }}</strong>
+                <span v-if="entry.chinese"> - {{ entry.chinese }}</span>
+                <p v-if="entry.examples">Example: {{ entry.examples }}</p>
+            </li>
+        </ul>
+        <p v-else>No vocabulary words saved yet.</p>
+    </div>
 </template>
-<style scoped>
 
+<style scoped>
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+
+li {
+    margin-bottom: 10px;
+}
 </style>
