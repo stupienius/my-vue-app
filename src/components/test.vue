85293c1e0bef63f5d6a0f2609f1f65157883a65d
stupienius <wangjay0401@gmail.com>
2024-07-26 16:45:38 +0800

    localstorage test staes

--- src/components/test.vue
+++ src/components/test.vue
@@ -1,27 +1,57 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 import Setting from './test_setting.vue'
 
-const testStatus = ref([false, false, false, false, false, false])
+const testStatus = ref([true, true, true, true, true, true])
+const wordList = ref([])
 
-const handleStatues = (component) => {
-    testStatus.value[component.which] = component.component
+function  storeSetting(){
+    localStorage.setItem('testSetting', JSON.stringify(testStatus.value))
 }
+
+function loadlibrary(){
+    const library = localStorage.getItem('vocabularyList')
+    if(library){
+        wordList = JSON.parse(library)
+    }
+}
+
+function loadSetting(){
+    const storedSettings = localStorage.getItem('testSetting')
+    if (storedSettings) {
+        testStatus.value = JSON.parse(storedSettings)
+    }
+}
+
+ 
+
+function handleStatuses(component){
+    testStatus.value[component.which] = component.component
+    storeSetting()
+}
+
+if (localStorage.getItem('testSetting') === null) {
+    storeSetting()
+}
+
+onMounted(() => {
+    loadSetting()
+})
 </script>
 
 <template>
     <div class="test">
         <div class="setting">
-            <Setting @setStatus="handleStatues" :switchStates="testStatus" />
+            <Setting @setStatus="handleStatuses" :switchStates="testStatus" />
         </div>
         <div class="quiz">
-            <p v-show="testStatus[2]" class="ch">chinese</p>
+            <p v-show="testStatus[2]" class="ch">stupienius</p>
             <p v-show="testStatus[3]" class="pos">(v)</p>
             <p v-show="testStatus[0]" class="head">h</p>
             <input type="text" id="test">
             <p v-show="testStatus[1]" class="tail">r</p>
             <p v-show="testStatus[5]" class="len">5</p>
-            <p v-show="testStatus[4]" class="sentance">you are the apple of my eyes</p>
+            <p v-show="testStatus[4]" class="sentence">you are the apple of my eyes</p>
         </div>
     </div>
 </template>
@@ -71,7 +101,7 @@ input{
     grid-row: 3/4;
     grid-column: 6/7;
 }
-.sentance{
+.sentence{
     border-radius: 5px;
     grid-row: 4/5;
     grid-column: 1/12;
